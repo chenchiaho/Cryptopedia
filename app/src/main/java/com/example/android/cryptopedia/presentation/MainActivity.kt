d31a2380d5ccf7e2c326b3b3d9eb04e35c6546a1
Kevin La <chenchiaho@gmail.com>
2021-11-23 02:17:29 +0800

    implemented UI for CoinDetail

--- app/src/main/java/com/example/android/cryptopedia/presentation/MainActivity.kt
+++ app/src/main/java/com/example/android/cryptopedia/presentation/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.android.cryptopedia.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.android.cryptopedia.R
+import com.example.android.cryptopedia.presentation.coin_detail.CoinDetailScreen
+import com.example.android.cryptopedia.presentation.coin_list.CoinListScreen
+import com.example.android.cryptopedia.presentation.ui.theme.CryptopediaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            CryptopediaTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
